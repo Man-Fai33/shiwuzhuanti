defb0969e021523bd050b2d8333e76123b3c3e31
Fai <56225561+Man-Fai33@users.noreply.github.com>
2022-02-15 01:50:59 +0800

    create project
    project and tailwind css

---
+++ website/src/App.js
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+import './App.css';
+import "tailwindcss/tailwind.css"
+function App() {
+  return (
+    <div class="min-h-screen bg-gray-50 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+  
+    <div class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+      <div class="max-w-md mx-auto">
+         
+        <div class="divide-y divide-gray-300/50">
+          <div class="py-8 text-base leading-7 space-y-6 text-gray-600">
+            <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+            <ul class="space-y-4">
+              <li class="flex items-center">
+                
+                <p class="ml-4">
+                  Customizing your
+                  <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                </p>
+              </li>
+              <li class="flex items-center">
+                 
+                <p class="ml-4">
+                  Extracting classes with
+                  <code class="text-sm font-bold text-gray-900">@apply</code>
+                </p>
+              </li>
+              <li class="flex items-center">
+                 
+                <p class="ml-4">Code completion with instant preview</p>
+              </li>
+            </ul>
+            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+          </div>
+          <div class="pt-8 text-base leading-7 font-semibold">
+            <p class="text-gray-900">Want to dig deeper into Tailwind?</p>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ website/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  purge: ['./src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
