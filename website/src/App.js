ddbe36a1bd2885d18e257a355a3274a4c6a4ec89
Fai <56225561+Man-Fai33@users.noreply.github.com>
2022-02-17 13:43:49 +0800

    fai
    Navbar

--- website/src/App.js
+++ website/src/App.js
@@ -1,48 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
 import "tailwindcss/tailwind.css"
+import {BrowserRouter,Routes,Route,Outlet} from 'react-router-dom';
+import Index from './page/Index';
+import NavBar from './components/Navber';
 function App() {
   return (
-    <div class="min-h-screen bg-gray-50 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
-  
-    <div class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
-      <div class="max-w-md mx-auto">
-         
-        <div class="divide-y divide-gray-300/50">
-          <div class="py-8 text-base leading-7 space-y-6 text-gray-600">
-            <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
-            <ul class="space-y-4">
-              <li class="flex items-center">
-                
-                <p class="ml-4">
-                  Customizing your
-                  <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
-                </p>
-              </li>
-              <li class="flex items-center">
-                 
-                <p class="ml-4">
-                  Extracting classes with
-                  <code class="text-sm font-bold text-gray-900">@apply</code>
-                </p>
-              </li>
-              <li class="flex items-center">
-                 
-                <p class="ml-4">Code completion with instant preview</p>
-              </li>
-            </ul>
-            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
-          </div>
-          <div class="pt-8 text-base leading-7 font-semibold">
-            <p class="text-gray-900">Want to dig deeper into Tailwind?</p>
-            <p>
-              
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className="container">
+    {/* <UserInfoContext.Provider  > */}
+      <BrowserRouter>
+      <NavBar>
+       {/* <NavBar setLogin={setLogin}> */}
+          {/* <NavBarItem to={"/upload"} text={"Upload PDF"} />
+          <NavBarItem to={"/pdflist"} text={"PDF List"} /> */}
+        </NavBar> 
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* <Route path="upload" element={<UploadPage />} />
+          <Route path="pdflist" element={<PDFListPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+      <Outlet />
+    {/* </UserInfoContext.Provider> */}
   </div>
+
   );
 }
 
